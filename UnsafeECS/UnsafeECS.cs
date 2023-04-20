213ad4c76918c036b675b6b013cdac86f2e16f47
SoSoReally <534605060@qq.com>
2023-04-20 13:30:53 +0800

    Update UnsafeECS.cs

--- UnsafeECS/UnsafeECS.cs
+++ UnsafeECS/UnsafeECS.cs
@@ -16,14 +16,28 @@ public  static partial class UnsafeCodeECS
         return new ReadOnlySpan<T>(dynamicBuffer.GetUnsafeReadOnlyPtr(), dynamicBuffer.Length);
     }
 
-    public unsafe static void UnSafeAddSpan<T>(this DynamicBuffer<T> dynamicBuffer, Span<T> list) where T : unmanaged
+    public unsafe static void UnSafeAddSpan<T>(this ref DynamicBuffer<T> dynamicBuffer, Span<T> list) where T : unmanaged
     {
+        if (list.Length <= 0) { return; }
         int elemSize = UnsafeUtility.SizeOf<T>();
         int oldLength = dynamicBuffer.Length;
         dynamicBuffer.ResizeUninitialized(oldLength + list.Length);
 
         byte* basePtr = (byte*)dynamicBuffer.GetUnsafePtr();
         UnsafeUtility.MemCpy(basePtr + (long)oldLength * elemSize, UnsafeUtility.AddressOf(ref list[0]), (long)elemSize * list.Length);
+    }
+    public unsafe static void AddRange<T>(this ref DynamicBuffer<T> dynamicBuffer, ReadOnlySpan<T> list) where T : unmanaged
+    {
+        if (list.Length <= 0) { return; }
+        int elemSize = UnsafeUtility.SizeOf<T>();
+        int oldLength = dynamicBuffer.Length;
+        dynamicBuffer.ResizeUninitialized(oldLength + list.Length);
+
+        byte* basePtr = (byte*)dynamicBuffer.GetUnsafePtr();
+        fixed (void* ptr = &list.GetPinnableReference())
+        {
+            UnsafeUtility.MemCpy(basePtr + (long)oldLength * elemSize, ptr, (long)elemSize * list.Length);
+        }
     }
     #region RefRO RefRW
     public unsafe static ref readonly T AsReadOnly<T>(this in ComponentLookup<T> componentLookup, in Entity entity) where T : unmanaged, IComponentData
@@ -35,7 +49,6 @@ public  static partial class UnsafeCodeECS
     {
         return ref componentLookup.GetRefRW(entity, false).ValueRW;
     }
-
     public unsafe static ref readonly T AsRWReadOnly<T>(this in ComponentLookup<T> componentLookup, in Entity entity) where T : unmanaged, IComponentData
     {
         return ref componentLookup.GetRefRW(entity, true).ValueRO;
